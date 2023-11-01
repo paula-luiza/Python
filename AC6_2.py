dc469d0eaea5a98a94956e2293ab174d636ab8f4
paula-luiza <142267093+paula-luiza@users.noreply.github.com>
2023-11-01 19:04:58 -0300

    Update AC6.py

--- AC6_2.py
+++ AC6_2.py
@@ -1,27 +1,42 @@
-"""
-import json
-    >>> obj = ['foo', {'bar': ['baz', None, 1.0, 2]}]
-    >>> json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]') == obj
-    True
-    >>> json.loads('"\\"foo\\bar"') == '"foo\x08ar'
-    True
-    >>> from io import StringIO
-    >>> io = StringIO('["streaming API"]')
-    >>> json.load(io)[0] == 'streaming API'
-    True
-Specializing JSON object 
-"""
+# EXERCICIO 1
+
+import random
+
+def roda_dado():
+    return random.randint(1, 6)
+
+def jogar_dados():
+    results= []
+    for cont in range(1, 100+1):
+        num = roda_dado()
+        results.append(num)
+    return results
+
+def conta_valores():
+    possibilidades = [1,2,3,4,5,6]
+    resultados = jogar_dados()
+    print(resultados)
+    for i in range(6):
+        y = possibilidades[i]
+        print(f'{y}: {resultados.count(y)} vezes;')
+    print('*' * 20)
+
+conta_valores()
+  
+
+# EXERCICIO 2
 
 import json
 
 def add_aluno(alunos, matricula, nome, email):
+    # adiciona aluno no dicionario
     alunos[matricula] = { 
         "nome": nome,
         "e-mail": email
     }
-    return alunos
 
 def pede_dados():
+    # verifica se arquivo existe e pede dados dos alunos
     try:
         with open('alunos.json', 'r') as file:
             dados_alunos = json.load(file)
@@ -30,7 +45,7 @@ def pede_dados():
 
 
     while True:
-        matricula = input('Digite o número da matrícula')
+        matricula = input('Digite o número da matrícula: ')
         nome = input('Digite o nome do aluno: ')
         email = input('Digite o email do aluno: ')
         if matricula == "" or nome == "" or email == "":
@@ -41,8 +56,26 @@ def pede_dados():
 
 
 def escrever_arquivo(dados_alunos):
+    # escreve dados no arquivo
     with open('alunos.json', 'w') as file:
-        json.dump(dados_alunos, file)
-        print('dados salvos')
+        json.dump(dados_alunos, file, indent=4)
+        print('Dados salvos.')
 
 pede_dados()
+
+
+# EXERCÍCIO 3
+
+from datetime import datetime, date
+import locale
+
+def recebe_data():
+    try:
+        locale.setlocale(locale.LC_TIME,'pt')
+        usuario = input('Digite uma data no formato DD/MM/AAAA: ')
+        data = datetime.strptime(usuario, "%d/%m/%Y")
+        print(data.strftime('%d de %B de %Y'))
+    except ValueError:
+        return None
+    
+recebe_data()
